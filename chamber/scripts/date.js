bee35662fc9bb2357a510bbd3fc888f404077d68
Murokarg <edwin.rene.rodriguez.ortiz@gmail.com>
2026-07-25 14:01:32 -0600

    deleted date.js, functionality moved to main.js

--- chamber/scripts/date.js
+++
@@ -1,7 +0,0 @@
-// add the year and the last modified date
-export const currentYear = new Date().getFullYear();
-export const lastModified = document.lastModified;
-
-// functions to add the year and the last modified date
-document.getElementById("currentYear").textContent = currentYear;
-document.getElementById("lastModified").textContent = document.lastModified;
